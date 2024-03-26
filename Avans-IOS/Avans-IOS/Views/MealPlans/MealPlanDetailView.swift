5206e2607dd77ec750c4255a87cca30e37b477da
Robin Pijnappels <73393404+RobinPijnappelsAvans@users.noreply.github.com>
2024-03-26 20:34:21 +0100

    added macro overview

--- Avans-IOS/Avans-IOS/Views/MealPlans/MealPlanDetailView.swift
+++ Avans-IOS/Avans-IOS/Views/MealPlans/MealPlanDetailView.swift
@@ -10,6 +10,24 @@ import SwiftUI
 struct MealPlanDetailView: View {
     var mealPlan: MealPlan
     
+    private func calculateTotalMacros() -> (calories: Int, protein: Int, fat: Int, carbohydrates: Int) {
+            var totalCalories = 0
+            var totalProtein = 0
+            var totalFat = 0
+            var totalCarbohydrates = 0
+
+            for mealNumber in mealPlan.mealNumbers {
+                for item in mealNumber.mealItems {
+                    totalCalories += item.standardMeal.calories
+                    totalProtein += item.standardMeal.protein
+                    totalFat += item.standardMeal.fat
+                    totalCarbohydrates += item.standardMeal.carbohydrates
+                }
+            }
+
+        return (totalCalories, totalProtein, totalFat, totalCarbohydrates)
+    }
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
@@ -18,10 +36,20 @@ struct MealPlanDetailView: View {
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .center)
 
-                // Example of displaying macros - adjust according to your actual data structure
-                Text("Macros Overview")
-                    .font(.headline)
-                    .frame(maxWidth: .infinity, alignment: .center)
+                let macros = calculateTotalMacros()
+                            VStack(alignment: .leading) {
+                                Text("Macros Overview")
+                                    .font(.headline)
+                                    .padding(.bottom, 2)
+                                Text("Calories: \(macros.calories)")
+                                Text("Protein: \(macros.protein)g")
+                                Text("Fat: \(macros.fat)g")
+                                Text("Carbs: \(macros.carbohydrates)g")
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(8)
 
                 ForEach(mealPlan.mealNumbers, id: \.id) { mealNumber in
                     VStack(alignment: .leading) {
